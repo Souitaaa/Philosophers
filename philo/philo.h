d65d5517ca79d31364f8a9238726b732bc3f8631
csouita <csouita@c4r2p5.1337.ma>
2024-08-10 12:40:43 +0100

    first push

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csouita <csouita@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/04 19:34:40 by csouita           #+#    #+#             */
+/*   Updated: 2024/08/04 19:39:26 by csouita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define  PHILO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *print_message_function( void *ptr );
+
+
+#endif 
